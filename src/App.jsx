648a02ef3b4f8970865ba03fd589b54489f73a33
Ksenia Agalakova <agalaksu111@gmail.com>
2024-01-25 00:26:31 +0300

    Fix layout for mobile screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import TodoForm from "./TodoForm";
 
 function App() {
   return (
-    <div className="flex justify-center items-center lg:h-screen">
-      <div className="lg:w-1/3">
+    <div className="flex justify-center m-4 lg:my-14">
+      <div className="w-full lg:w-1/3">
         <h2 className="text-center font-medium text-2xl">DAILIST</h2>
-        <div className="w-full lg:max-h-96">
+        <div className="w-full">
           <TodoForm></TodoForm>
         </div>
-        <div className="lg:max-h-96 overflow-visible rounded-md border-gray-300">
+        <div className="overflow-visible rounded-md border-gray-300">
           <TaskBoard></TaskBoard>
         </div>
       </div>
